878309d0d5493feabfbc84fc04528f494dfdc72e
Daniel Xavier <dxdanielxavier@gmail.com>
2018-06-23 11:49:01 -0300

    Current shelf as defaultValue

--- src/Book.js
+++ src/Book.js
@@ -15,7 +15,7 @@ class Book extends Component {
           <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
             <div className="book-shelf-changer">
-              <select defaultValue={book.shelf} onChange={(e) => onUpdate(book, e.target.value)}>
+              <select defaultValue={book.shelf || 'none'} onChange={(e) => onUpdate(book, e.target.value)}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
